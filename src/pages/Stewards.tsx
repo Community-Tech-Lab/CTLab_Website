bbb93488eb888d8d940497d4cc606bc7250eb9d6
SkyMazter <comunidadoscar@gmail.com>
2024-12-12 16:29:39 -0500

    updated stewards page

--- src/pages/Stewards.tsx
+++ src/pages/Stewards.tsx
@@ -1,5 +1,18 @@
+import bannerPicture from "../assets/visit_banner.svg";
+import Menu from "../components/Menu.tsx";
+import Banner from "../components/Banner.tsx";
+
 const Stewards = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Menu />
+      <Banner
+        bannerText="Meet the Stewards"
+        bannerColor="red"
+        image={bannerPicture}
+      />
+    </div>
+  );
 };
 
 export default Stewards;
