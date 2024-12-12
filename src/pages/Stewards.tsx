1ed6f69cde6e4eae26eae57674880f57aa8e12bb
SkyMazter <comunidadoscar@gmail.com>
2024-12-12 17:09:35 -0500

    added new component

--- src/pages/Stewards.tsx
+++ src/pages/Stewards.tsx
@@ -1,17 +1,20 @@
 import bannerPicture from "../assets/visit_banner.svg";
 import Menu from "../components/Menu.tsx";
 import Banner from "../components/Banner.tsx";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const Stewards = () => {
   return (
-    <div>
+    <Container>
       <Menu />
       <Banner
         bannerText="Meet the Stewards"
         bannerColor="red"
         image={bannerPicture}
       />
-    </div>
+      <Row></Row>
+    </Container>
   );
 };
 
